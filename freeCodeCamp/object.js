d2ffc24fb09feb60bcbec693c70d81746c7b6a1d
moinulislam <moinul.islam@enosisbd.com>
2022-01-07 02:27:20 +0600

    second

--- freeCodeCamp/object.js
+++ freeCodeCamp/object.js
@@ -1,65 +1,65 @@
 // class 20:
-var obj = {
-    "name" : "Moin",
-    "height": 6,
-    "role": ["QA", "RA"],
-    "favourite drinks": "beer"
-};
-console.log(obj.height);               // accessing values of objects
-console.log(obj.name);
-console.log(obj.role[0]);
-console.log(obj["favourite drinks"]);  // when we need to access a key which has space in the middle  
-obj.height = 5.11;                     // manipulating values of object
-console.log(obj.height);
+// var obj = {
+//     "name" : "Moin",
+//     "height": 6,
+//     "role": ["QA", "RA"],
+//     "favourite drinks": "beer"
+// };
+// console.log(obj.height);               // accessing values of objects
+// console.log(obj.name);
+// console.log(obj.role[0]);
+// console.log(obj["favourite drinks"]);  // when we need to access a key which has space in the middle  
+// obj.height = 5.11;                     // manipulating values of object
+// console.log(obj.height);
 
 // class 21:
-var testObj = {
-    10 : "hey",
-    11 : "hi"
-};
-var a = 10;
-var word = testObj[a];                // accessing key of object with variable
-console.log(word);
+// var testObj = {
+//     10 : "hey",
+//     11 : "hi"
+// };
+// var a = 10;
+// var word = testObj[a];                // accessing key of object with variable
+// console.log(word);
 
 // class 22:
-var o = {
-    "name" : "dihan",
-    "role" : "meku biral",
-    "ex" : "rifat"
-};
-o.vg = "no";                         // adding keys to the object
-console.log(o);
-delete o.vg;                         // deleting keys from object
-console.log(o)
+// var o = {
+//     "name" : "dihan",
+//     "role" : "meku biral",
+//     "ex" : "rifat"
+// };
+// o.vg = "no";                         // adding keys to the object
+// console.log(o);
+// delete o.vg;                         // deleting keys from object
+// console.log(o)
 
 // class 23:
-function a(val){
-    var location = {
-        "moin" : "ctg",
-        "joy" : "ctg",
-        "raihan" : "dhaka",
-        "rifat" : "london"
-    };
-    return location[val];
-}
-console.log(a('rifat'));
+// function a(val){
+//     var location = {
+//         "moin" : "ctg",
+//         "joy" : "ctg",
+//         "raihan" : "dhaka",
+//         "rifat" : "london"
+//     };
+//     return location[val];
+// }
+// console.log(a('rifat'));
 
 //class 24:
-var a = {
-    "job" : "qa",
-    "company" : "enosis",
-    "member" : 321
-};
+// var a = {
+//     "job" : "qa",
+//     "company" : "enosis",
+//     "member" : 321
+// };
 
-function checkObj(property){
-    if (a.hasOwnProperty(property)){
-        return a[property];
-    }
-    else{
-        return "not found";
-    }
-}
-console.log(checkObj('job'));
+// function checkObj(property){
+//     if (a.hasOwnProperty(property)){
+//         return a[property];
+//     }
+//     else{
+//         return "not found";
+//     }
+// }
+// console.log(checkObj('job'));
 
 // class 25:
 
@@ -72,7 +72,11 @@ var a = [
         "world tour": [
             "LA",
             "NYC",
-            "FL"
-        ]
+            "FL",
+            {"a":"miami"}
+        ],
+        "fund": "500$ Million"
     }
 ]
+console.log(a[0]["world tour"][3].a); // when there is space, put that into [ bracket ]. access list with [0], access object with .a
+
